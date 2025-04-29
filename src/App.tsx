58d95aa8e0b07e3554cabb1dfadb6e8a3d29328d
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-04-29 23:26:41 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './css/App.css'
+
+function App(){
+  return(
+  <>
+    <h1 className="flex justify-center items-center w-[100vw] h-[100vh]">Payroll System</h1>
+  </>
+  )
+}
+
+export default App
