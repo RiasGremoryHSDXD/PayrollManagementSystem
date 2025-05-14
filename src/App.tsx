3523e6bc14159b0e43bfbd3fc60894408c1b5560
GomezJohn111 <goemz.johnandre023@gmail.com>
2025-05-14 22:41:17 +0800

    Attendance UI almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,47 @@
-import './css/App.css'
-import LogInForm from './pages/LogInForm/index'
-import EmployeeLogIn from './pages/DashBoard/Employee/index'
-import { Routes, Route, Navigate } from 'react-router-dom';
-import ProtectedRoute from '../src/auth/ProtectedRoute';
-import AttendanceForm from './attendance/components/AttendanceForm'
+import "./css/App.css";
+import LogInForm from "./pages/LogInForm/index";
+import EmployeeLogIn from "./pages/DashBoard/Employee/index";
+import { Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "../src/auth/ProtectedRoute";
+import AttendanceForm from "./pages/attendance/components/AttendanceForm";
 
-function App(){
-  return(
+function App() {
+  return (
     <Routes>
-
       {/*Log In Form Componets*/}
-      <Route path="/" element={
-        <div className="flex justify-center items-center h-screen bg-slate-200">
-          <LogInForm />
-        </div>        
-      } />
+      <Route
+        path="/"
+        element={
+          <div className="flex justify-center items-center h-screen bg-slate-200">
+            <LogInForm />
+          </div>
+        }
+      />
 
       {/*Employee Dashboard*/}
-      <Route path="/employee" element={
-        <ProtectedRoute>
-          <EmployeeLogIn />
-        </ProtectedRoute>
-      } />
+      <Route
+        path="/employee"
+        element={
+          <ProtectedRoute>
+            <EmployeeLogIn />
+          </ProtectedRoute>
+        }
+      />
 
       {/*Attendance Form*/}
       {/*Log In Form Componets*/}
-      <Route path="/" element={
-        <div className="flex justify-center items-center h-screen bg-slate-200">
-          <AttendanceForm />
-        </div>        
-      } />
+      <Route
+        path="/"
+        element={
+          <div className="flex justify-center items-center h-screen bg-slate-200">
+            <AttendanceForm />
+          </div>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { HashRouter } from 'react-router-dom';   // ← change here
-import { AuthProvider } from './auth/AuthContext'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { HashRouter } from "react-router-dom"; // ← change here
+import { AuthProvider } from "./auth/AuthContext";
 
-
-
-createRoot(document.getElementById('root')!).render(
-  <HashRouter>                                  
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </HashRouter>,
-)
+  </HashRouter>
+);
