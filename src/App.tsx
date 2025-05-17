b8d51f48f35ba28e30a6b808a4c29a63282d4ec6
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-05-17 19:23:52 +0800

    modify files

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import LogInForm from "./pages/LogInForm/index";
 import DashBoardEmployee from "./pages/Dashboard/Employee";
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "../src/auth/ProtectedRoute";
-import Payroll from "./pages/Payroll/components/payroll"
 
 function App() {
   return (
@@ -24,18 +23,6 @@ function App() {
         element={
           <ProtectedRoute>
             <DashBoardEmployee />
-          </ProtectedRoute>
-        }
-      />
-
-      {/*Payroll Form*/}
-      <Route
-        path="/payroll"
-        element={
-          <ProtectedRoute>
-            <div className="flex justify-center items-center h-screen bg-slate-200">
-              <Payroll />
-            </div>
           </ProtectedRoute>
         }
       />
